f2fe64ea4974189b2f3014bd66fd8928bfc1f698
Piotr Wittchen <piotr@wittchen.biz.pl>
2016-01-13 19:57:37 +0100

    reorganizing project structure

--- ubuntu/development/configure.sh
+++ ubuntu/development/configure.sh
@@ -14,19 +14,19 @@ main() {
   msg "-------- preparing Look & Feel of JetBrains IDEs --------"  
 
   msg "copying Darcula-modified theme to JetBrains IDEs directories"
-  cp config/Darcula-modified.icls $HOME/.IdeaIC*/config/colors/
-  cp config/Darcula-modified.icls $HOME/.AndroidStudio*/config/colors/
-  cp config/Darcula-modified.icls $HOME/.PyCharm*/config/colors/
+  cp Darcula-modified.icls $HOME/.IdeaIC*/config/colors/
+  cp Darcula-modified.icls $HOME/.AndroidStudio*/config/colors/
+  cp Darcula-modified.icls $HOME/.PyCharm*/config/colors/
 
   msg "copying color scheme to JetBrains IDEs directories"
-  cp config/color.scheme.xml $HOME/.IdeaIC*/config/options/color.scheme.xml
-  cp config/color.scheme.xml $HOME/.AndroidStudio*/config/options/color.scheme.xml
-  cp config/color.scheme.xml $HOME/.PyCharm*/config/options/color.scheme.xml
+  cp color.scheme.xml $HOME/.IdeaIC*/config/options/color.scheme.xml
+  cp color.scheme.xml $HOME/.AndroidStudio*/config/options/color.scheme.xml
+  cp color.scheme.xml $HOME/.PyCharm*/config/options/color.scheme.xml
 
   msg "copying custom UI config to JetBrains IDEs directories"
-  cp config/ui.inf.xml $HOME/.IdeaIC*/config/options/ui.inf.xml
-  cp config/ui.inf.xml $HOME/.AndroidStudio*/config/options/ui.inf.xml
-  cp config/ui.inf.xml $HOME/.PyCharm*/config/options/ui.inf.xml
+  cp ui.inf.xml $HOME/.IdeaIC*/config/options/ui.inf.xml
+  cp ui.inf.xml $HOME/.AndroidStudio*/config/options/ui.inf.xml
+  cp ui.inf.xml $HOME/.PyCharm*/config/options/ui.inf.xml
 
   # you can setup your own paths below
   export PATH_TO_ANDROID_STUDIO=$HOME/Development/android/android-studio
@@ -34,9 +34,9 @@ main() {
   export PATH_TO_PYCHARM=$HOME/Development/python/pycharm-ce
 
   msg "copying run scripts to development directories"
-  cp scripts/run-studio.sh $PATH_TO_ANDROID_STUDIO/bin/run-studio.sh
-  cp scripts/run-idea.sh $PATH_TO_INTELLIJ_IDEA/bin/run-idea.sh
-  cp scripts/run-pycharm.sh $PATH_TO_PYCHARM/bin/run-pycharm.sh
+  cp run-studio.sh $PATH_TO_ANDROID_STUDIO/bin/run-studio.sh
+  cp run-idea.sh $PATH_TO_INTELLIJ_IDEA/bin/run-idea.sh
+  cp run-pycharm.sh $PATH_TO_PYCHARM/bin/run-pycharm.sh
 
   msg "DONE!"
 }
