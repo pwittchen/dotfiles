e958048a19a92bffaad853e0a8fea748bdd5b3bd
Piotr Wittchen <piotr@wittchen.biz.pl>
2016-05-16 17:06:04 +0200

    Update createPkupDiff.sh

--- osx/scripts/createPkupDiff.sh
+++ osx/scripts/createPkupDiff.sh
@@ -10,6 +10,14 @@
 
 set -e
 
+# info about developer
+surname=Wittchen
+name=Piotr
+
+# info about destination directory
+dateFormat=$(date +'%Y%m')
+filePath=Documents/hybris/pkup/raporty/doc/"$surname"_"$name"_"$dateFormat"
+
 msg() {
   tput setaf 2 # green text
   echo ">>> $1"
@@ -21,16 +29,11 @@ saveDiff() {
   msg "saving diff for $1"
   cd $yHYBRIS_SRC/"$1"
   gitUserName=$(git config user.name)
-  dateFormat=$(date +'%Y%m')
-  filePath=Documents/hybris/pkup/raporty/doc/Wittchen_Piotr_"$dateFormat"
   mkdir -p ~/"$filePath"
-  git log --author="$gitUserName" -p --since="20 days ago" > ~/"$filePath"/"$1".diff
+  git log --author="$gitUserName" -p --since="20 days" > ~/"$filePath"/"$1".diff
 }
 
 removeDiffIfEmpty() {
-  dateFormat=$(date +'%Y%m')
-  filePath=Documents/hybris/pkup/raporty/doc/Wittchen_Piotr_"$dateFormat"
-
   if [ ! -s ~/"$filePath"/"$1".diff ]
   then
     msg "no changes performed in $1 -> removing empty $1.diff file"
