ef209e686696c5f3e36519294780717baf4c3afa
Piotr Wittchen <piotr@wittchen.biz.pl>
2016-05-16 17:17:29 +0200

    Update createPkupDiff.sh

--- osx/scripts/createPkupDiff.sh
+++ osx/scripts/createPkupDiff.sh
@@ -14,9 +14,10 @@ set -e
 surname=Wittchen
 name=Piotr
 
-# info about destination directory
+# info about destination directory and files
 dateFormat=$(date +'%Y%m')
-filePath=Documents/hybris/pkup/raporty/doc/"$surname"_"$name"_"$dateFormat"
+filePath=~/Documents/hybris/pkup/raporty/doc/"$surname"_"$name"_"$dateFormat"
+fileExtension=diff
 
 msg() {
   tput setaf 2 # green text
@@ -29,15 +30,15 @@ saveDiff() {
   msg "saving diff for $1"
   cd $yHYBRIS_SRC/"$1"
   gitUserName=$(git config user.name)
-  mkdir -p ~/"$filePath"
-  git log --author="$gitUserName" -p --since="20 days" > ~/"$filePath"/"$1".diff
+  mkdir -p "$filePath"
+  git log --author="$gitUserName" -p --since="20 days" > "$filePath"/"$1"."$fileExtension"
 }
 
 removeDiffIfEmpty() {
-  if [ ! -s ~/"$filePath"/"$1".diff ]
+  if [ ! -s "$filePath"/"$1"."$fileExtension" ]
   then
-    msg "no changes performed in $1 -> removing empty $1.diff file"
-    rm -f ~/"$filePath"/"$1".diff
+    msg "no changes performed in $1 -> removing empty $1.$fileExtension file"
+    rm -f "$filePath"/"$1".$fileExtension
   fi
 }
 
