0678709b54133fa1f8c82fd53e8e8d9af4e9dac8
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-01-13 12:43:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -e
-# copy configuration files
 sudo cp .gitconfig ~/
 sudo cp .gitignore_global ~/
 sudo cp .hybris.conf ~/
 sudo cp .tmux.conf ~/
 sudo cp .vimrc ~/
 sudo cp .zshrc ~/
-# copy scripts
 sudo cp scripts/* /usr/local/bin/
-# reload shell
-source ~/.zshrc
 echo "dotfiles installed successfully! \o/"
