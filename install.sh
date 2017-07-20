71c551a680d1a547592bc7223b8d33d15e0cf241
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-07-20 22:33:26 +0200

    renaming scripts dir to .scripts, updating access rights of the files and install.sh script

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo cp .hybris.conf ~/
 sudo cp .tmux.conf ~/
 sudo cp .vimrc ~/
 sudo cp .zshrc ~/
-sudo cp scripts/* /usr/local/bin/
+sudo cp .ghci ~/
+sudo cp .scripts/* /usr/local/bin/
 echo "dotfiles installed successfully! \o/"
 echo "run source ~/.zshrc command manually to refresh your configuration"
