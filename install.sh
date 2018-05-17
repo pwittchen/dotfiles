61be6c1b09d8ce5c20409534f9a5ff180c28acc7
Piotr Wittchen <piotr@wittchen.io>
2018-05-17 17:39:43 +0200

    updating install script to make it valid for different operating systems

--- install.sh
+++ install.sh
@@ -2,10 +2,16 @@
 set -e
 sudo cp .gitconfig ~/
 sudo cp .gitignore_global ~/
-sudo cp .hybris.conf ~/
 sudo cp .tmux.conf ~/
 sudo cp .vimrc ~/
 sudo cp .zshrc ~/
 sudo cp .ghci ~/
+
+# I'm using hybris config on macOS only
+
+if [ `uname` = "Darwin"  ]; then
+  sudo cp .hybris.conf ~/
+fi
+
 echo "dotfiles installed successfully! \o/"
 echo "run source ~/.zshrc command manually to refresh your configuration"
