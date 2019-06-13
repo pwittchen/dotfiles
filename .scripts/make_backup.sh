47b5d020af8643df1fb3f2902a58b3d5bd08812e
Piotr Wittchen <piotr@wittchen.io>
2019-06-14 00:47:17 +0200

    adding .scripts dir

---
+++ .scripts/make_backup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+source ~/.config/scripts/make_backup.conf
+
+echo "starting backup process"
+
+rm -rf ~/Backup/*
+
+echo "preparing directories to backup"
+
+cp -avr ~/Dokumenty/ebooks ~/Backup/ebooks
+cp -avr ~/Dokumenty/finance ~/Backup/finance
+cp -avr ~/Dokumenty/gym ~/Backup/gym
+cp -avr ~/Dokumenty/licenses ~/Backup/licenses
+cp -avr ~/Dokumenty/other ~/Backup/other
+cp -avr ~/Dokumenty/presentations ~/Backup/presentations
+cp -avr ~/Dokumenty/secret ~/Backup/secret
+cp -avr ~/Dokumenty/sonatype ~/Backup/sonatype
+cp -avr ~/Dokumenty/travel ~/Backup/travel
+cp -avr ~/Dokumenty/work ~/Backup/work
+
+echo "uploading data to the FTP server"
+
+lftp ftp://$USER:$PASS@$HOST -e "set ftp:ssl-allow no; mirror -R -v --delete-first ~/Backup backup; quit"
+
+date >> ~/Dokumenty/logs/ftp_backup.log
+
+echo "backup finished"
