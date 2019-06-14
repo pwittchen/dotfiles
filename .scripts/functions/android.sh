9716f0da644074eee2f24ffcb097cdce24814a76
Piotr Wittchen <piotr@wittchen.io>
2019-06-14 11:14:45 +0200

    adding android platform tools to the path

--- .scripts/functions/android.sh
+++ .scripts/functions/android.sh
@@ -2,6 +2,7 @@
 
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$ANDROID_HOME/emulator
 
 function listAndroidEmulators() {
