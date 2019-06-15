a0e879cfb5c0f1abddc5161f4c39f331cd9ee44f
Piotr Wittchen <piotr@wittchen.io>
2019-06-15 23:50:11 +0200

    added reading temperature of the processor

--- .argos/01_cpu.60s.sh
+++ .argos/01_cpu.60s.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-mpstat | awk '$12 ~ /[0-9.]+/ { print "CPU " 100 - $12"%" }'
+cpu_usage=$(mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12"%" }')
+cpu_temp=$(sensors | grep Package | awk '{print $4}')
+echo "CPU $cpu_usage ($cpu_temp)"
