fd428f02b15935c9f06288da4a48f2b29841727e
Piotr Wittchen <piotr@wittchen.io>
2019-06-16 02:30:04 +0200

    removing unused code

--- .scripts/functions/linux.sh
+++ .scripts/functions/linux.sh
@@ -35,28 +35,12 @@ function listMemoryIntensivePs() {
   ps axch -o cmd:15,%mem --sort=-%mem | head
 }
 
-function get_ip_wlan() {
-  no_of_broadcasts="$(ifconfig | grep broadcast | wc -l)"
-  if [ $no_of_broadcasts = "2" ]; then
-    ip="$(ifconfig | grep broadcast | awk '{print $2}' | tail -n1)"
-  fi
-}
-
-function get_ip_eth() {
-  ip="$(ifconfig | grep ether | awk '{print $2}')"
-  if [[ $ip != *"."* ]]; then
-    ip=""
-  fi
-}
-
 function showIp() {
   # get ip of wlan
   no_of_broadcasts="$(ifconfig | grep broadcast | wc -l)"
   if [ $no_of_broadcasts = "2" ]; then
     ip="$(ifconfig | grep broadcast | awk '{print $2}' | tail -n1)"
   fi
-
-
   if [ ! -z "$ip" ]; then
     echo "$ip"
   else
