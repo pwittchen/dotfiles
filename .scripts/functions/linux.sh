ed08ab2c2f05f16e4a36bc1f1e6ee48ce2f22f73
Piotr Wittchen <piotr@wittchen.io>
2019-06-16 02:27:49 +0200

    adding function for showing ip and mac addresses

--- .scripts/functions/linux.sh
+++ .scripts/functions/linux.sh
@@ -34,3 +34,45 @@ function listCpuIntensivePs() {
 function listMemoryIntensivePs() {
   ps axch -o cmd:15,%mem --sort=-%mem | head
 }
+
+function get_ip_wlan() {
+  no_of_broadcasts="$(ifconfig | grep broadcast | wc -l)"
+  if [ $no_of_broadcasts = "2" ]; then
+    ip="$(ifconfig | grep broadcast | awk '{print $2}' | tail -n1)"
+  fi
+}
+
+function get_ip_eth() {
+  ip="$(ifconfig | grep ether | awk '{print $2}')"
+  if [[ $ip != *"."* ]]; then
+    ip=""
+  fi
+}
+
+function showIp() {
+  # get ip of wlan
+  no_of_broadcasts="$(ifconfig | grep broadcast | wc -l)"
+  if [ $no_of_broadcasts = "2" ]; then
+    ip="$(ifconfig | grep broadcast | awk '{print $2}' | tail -n1)"
+  fi
+
+
+  if [ ! -z "$ip" ]; then
+    echo "$ip"
+  else
+    # get ip of eth
+    ip="$(ifconfig | grep ether | awk '{print $2}')"
+    if [[ $ip != *"."* ]]; then
+      ip=""
+    fi
+    if [ ! -z "$ip" ]; then
+      echo "$ip"
+    else
+      echo "not connected"
+    fi
+  fi
+}
+
+function showMac() {
+  ip -o link show dev wlp58s0 | awk '{print $17}'
+}
