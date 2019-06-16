b98215cac29dc3b3ff15210732021028b729540d
Piotr Wittchen <piotr@wittchen.io>
2019-06-16 20:24:12 +0200

    improving/simplifying disk usage argos script

--- .argos/03_ssd.300s.sh
+++ .argos/03_ssd.300s.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-disk_used_percentage=$(df | tail -n 1 | awk '{print $5}')
-disk_used_bytes=$(df | tail -n 1 | awk '{print $3}')
-disk_used_gb=`echo ${disk_used_bytes} / 1000000 | bc -l | cut -f1 -d"."`
-disk_free_bytes=$(df | tail -n 1 | awk '{print $4}')
-disk_free_gb=`echo ${disk_free_bytes} / 1000000 | bc -l | cut -f1 -d"."`
-disk_capacity_gb=$[disk_used_gb+disk_free_gb]
-
-echo "SSD $disk_used_percentage"
+disk_used_percentage=$(df -Th | grep ^/dev/n | awk '{print $6}' | rev | cut -c 2- | rev | paste -sd+ | bc)
+echo "SSD $disk_used_percentage%"
 echo "---"
 echo "disk usage:"
-echo "$disk_used_gb GB / $disk_capacity_gb GB"
-echo "excluding boot partition"
+df -Th | grep ^/dev/n
