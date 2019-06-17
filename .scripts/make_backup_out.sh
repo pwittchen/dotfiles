2f75264ce05c701b73f6496c7af0616ab2af24a7
Piotr Wittchen <piotr@wittchen.io>
2019-06-17 23:08:09 +0200

    backing up scripts config

--- .scripts/make_backup_out.sh
+++ .scripts/make_backup_out.sh
@@ -14,5 +14,6 @@ cp -avr ~/Obrazy/wallpapers ~/Backup/wallpapers
 mkdir -p ~/Backup/secrets
 cp -avr ~/.gnupg ~/Backup/secrets/.gnupg
 cp -avr ~/.password-store ~/Backup/secrets/.password-store
+cp -avr ~/.config/scripts ~/Backup/secrets/.configscripts
 lftp ftp://$USER:$PASS@$HOST -e "set ftp:ssl-allow no; mirror -R -v --delete-first ~/Backup backup; quit"
 date >> ~/Dokumenty/logs/ftp_backup_out.log
