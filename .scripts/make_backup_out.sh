e7fc471c140ce340c9bd294dc895238d5b10682f
Piotr Wittchen <piotr@wittchen.io>
2019-06-17 22:56:45 +0200

    updating backup script

--- .scripts/make_backup_out.sh
+++ .scripts/make_backup_out.sh
@@ -7,12 +7,12 @@ cp -avr ~/Dokumenty/gym ~/Backup/gym
 cp -avr ~/Dokumenty/licenses ~/Backup/licenses
 cp -avr ~/Dokumenty/other ~/Backup/other
 cp -avr ~/Dokumenty/presentations ~/Backup/presentations
-cp -avr ~/Dokumenty/secret ~/Backup/secret
 cp -avr ~/Dokumenty/tmp ~/Backup/tmp
 cp -avr ~/Dokumenty/travel ~/Backup/travel
 cp -avr ~/Dokumenty/work ~/Backup/work
 cp -avr ~/Obrazy/wallpapers ~/Backup/wallpapers
-cp -avr ~/.gnupg ~/Backup/secret/.gnupg
-cp -avr ~/.password-store ~/Backup/secret/.password-store
+mkdir -p ~/Backup/secrets
+cp -avr ~/.gnupg ~/Backup/secrets/.gnupg
+cp -avr ~/.password-store ~/Backup/secrets/.password-store
 lftp ftp://$USER:$PASS@$HOST -e "set ftp:ssl-allow no; mirror -R -v --delete-first ~/Backup backup; quit"
 date >> ~/Dokumenty/logs/ftp_backup_out.log
