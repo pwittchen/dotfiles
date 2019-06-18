1e62368b7702ca22e91c41d4da686a71040d6b20
Piotr Wittchen <piotr@wittchen.io>
2019-06-18 17:12:36 +0200

    updating script for pics backup

--- .scripts/make_backup_disk_pics.sh
+++ .scripts/make_backup_disk_pics.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 disk_name=$(ls /media/`whoami` | tail -1)
-if [ $disk_name != "" ]; then
+if [ "$disk_name" != "" ]; then
   sudo cp -avru ~/Obrazy/photos/* /media/$(whoami)/$disk_name/photos/
   date >> ~/Dokumenty/logs/disk_backup_pics.log
 else
