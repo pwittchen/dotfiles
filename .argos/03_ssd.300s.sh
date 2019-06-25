f2e09028adc20b0e75cad807d3217dce9fa2bdea
Piotr Wittchen <piotr.wittchen@enginiety.com>
2019-06-25 11:55:11 +0200

    adding disk type reading in argos script

--- .argos/03_ssd.300s.sh
+++ .argos/03_ssd.300s.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 disk_used_percentage=$(df -Th | grep ^/dev/n | awk '{print $6}' | rev | cut -c 2- | rev | paste -sd+ | bc)
-echo "SSD $disk_used_percentage%"
+
+if [ $(cat /sys/block/n*/queue/rotational) == "0" ]; then
+  disk_type="SSD"
+else
+  disk_type="HDD"
+fi
+
+echo "$disk_type $disk_used_percentage%"
 echo "---"
 echo "disk usage:"
 df -Th | grep ^/dev/n
