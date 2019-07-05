51e794758cd3a3f0bc1b5febf504dba946e29b84
Piotr Wittchen <piotr@wittchen.io>
2019-07-05 17:10:16 +0200

    fixing host var in ftp backup scripts

--- .scripts/make_backup_ftp_out.sh
+++ .scripts/make_backup_ftp_out.sh
@@ -17,5 +17,5 @@ mkdir -p ~/Backup/secrets
 cp -avr ~/.gnupg ~/Backup/secrets/.gnupg
 cp -avr ~/.password-store ~/Backup/secrets/.password-store
 cp -avr ~/.config/scripts ~/Backup/secrets/.configscripts
-/usr/bin/lftp ftp://$SERVER_LINUX_PL_USER:$SERVER_LINUX_PL_PASS@$SERVER_LINUXPL_HOST -e "set ftp:ssl-allow no; mirror -R -v --delete-first ~/Backup backup; quit"
+/usr/bin/lftp ftp://$SERVER_LINUX_PL_USER:$SERVER_LINUX_PL_PASS@$SERVER_LINUX_PL -e "set ftp:ssl-allow no; mirror -R -v --delete-first ~/Backup backup; quit"
 date >> /var/log/scripts/ftp_backup_out.log
