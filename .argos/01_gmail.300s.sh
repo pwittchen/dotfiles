4e88cb922ea695757a7a8b8c3345d3507c9027d9
Piotr Wittchen <piotr@wittchen.io>
2019-07-16 19:08:29 +0200

    adding mechanism to prevent sending multiple notification about new e-mail if the same notification was already sent and e-mails are still not read

--- .argos/01_gmail.300s.sh
+++ .argos/01_gmail.300s.sh
@@ -11,9 +11,16 @@ fi
 
 msg_count=$(curl -u "$GMAIL_USER:$GMAIL_PASS" -s 'https://mail.google.com/mail/feed/atom' | xmllint --format - | grep fullcount | sed -e 's/<[^>]*>//g' | sed 's/ //g')
 
+# tmp file is created to prevent sending the same notification multiple times
+tmp_file="/tmp/email_notification_sent"
+
 if [ "$msg_count" == "0" ] ; then
   echo "📪 $msg_count"
+  rm -f "$tmp_file" || true
 else 
   echo "📫 $msg_count"
-  notify-send "e-mail" "you have $msg_count new message(s)"
+  if [ ! -f "$tmp_file" ] ; then
+    notify-send "e-mail" "you have $msg_count new message(s)"
+    touch "$tmp_file"
+  fi
 fi
