ec1caf9c1be98f9011a516dcfb00e299609a59e0
Piotr Wittchen <piotr@wittchen.io>
2019-07-21 00:59:27 +0200

    updating aliases

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-alias dvp="cd ~/Development 	&& ls -a"
-alias dox="cd ~/Documents 	&& ls -a"
-alias dld="cd ~/Downloads 	&& ls -a"
-alias pix="cd ~/Pictures	&& ls -a"
+alias dvp="cd ~/Development && ls -a"
+alias dox="cd ~/Documents   && ls -a"
+alias dld="cd ~/Downloads   && ls -a"
+alias pix="cd ~/Pictures    && ls -a"
+alias kzm="libreoffice ~/Documents/finance/budzet/2019/koszty-zmienne.ods"
+alias kst="libreoffice ~/Documents/finance/budzet/2019/koszty-stale.ods"
+alias bdt="libreoffice ~/Documents/finance/budzet/2019/budzet.ods"
