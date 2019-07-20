369257df7fa2b7f52be5699cb7fd4b8c0748835d
Piotr Wittchen <piotr@wittchen.io>
2019-07-20 23:11:27 +0200

    updating rss emoji

--- .argos/03_rss.300s.sh
+++ .argos/03_rss.300s.sh
@@ -3,5 +3,5 @@ no_of_unread_arts=$(newsboat -x print-unread | awk '{print $1}')
 if [ "$no_of_unread_arts" == "" ] ; then
   echo " "
 else
-  echo "🗞️ $no_of_unread_arts"
+  echo "📶 $no_of_unread_arts"
 fi
