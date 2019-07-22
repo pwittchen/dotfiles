70b6d31c5af5207721227a3a565a72853287f767
Piotr Wittchen <piotr.wittchen@enginiety.com>
2019-07-22 11:25:50 +0200

    updating play scripts -> fixing paths, importing necessary scripts

--- .scripts/play_clean_build.sh
+++ .scripts/play_clean_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source $HOME/.config/scripts/enginiety.conf
-cd $PLAY_BLD
+cd $PLAY_HYB
+. ./setantenv.sh
 ant clean all
 cd -
--- .scripts/play_clean_build_run.sh
+++ .scripts/play_clean_build_run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 source $HOME/.config/scripts/enginiety.conf
-cd $PLAY_BLD
+cd $PLAY_HYB
+. ./setantenv.sh
 ant clean all
 ./hybrisserver.sh debug
 cd -
