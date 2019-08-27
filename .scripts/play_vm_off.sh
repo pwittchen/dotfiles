b2607dca3b2f939bb2e1e64bb39f47078c6156de
Piotr Wittchen <piotr.wittchen@enginiety.com>
2019-08-27 10:28:12 +0200

    adding messages for work scripts for starting/stopping virtual machine

--- .scripts/play_vm_off.sh
+++ .scripts/play_vm_off.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+echo "stopping virtual machine"
 VBoxManage controlvm "OraclePlay12_1808" poweroff --type headless
+echo "done"
--- .scripts/play_vm_on.sh
+++ .scripts/play_vm_on.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+echo "starting virtual box"
 VBoxManage startvm "OraclePlay12_1808" --type headless
+echo "done"
