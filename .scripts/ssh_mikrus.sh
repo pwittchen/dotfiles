bc52d127814eecc4641f622520fb729beaebfa10
Piotr Wittchen <piotr@wittchen.io>
2019-12-20 10:54:51 +0100

    renaming scripts

--- .scripts/ssh_mikrus.sh
+++ .scripts/ssh_mikrus.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 source ~/.config/scripts/mikrus.conf
 
-ssh -l $SERVER_MIKRUS_USER \
-    -p $SERVER_MIKRUS_SSH_PORT \
-    $SERVER_MIKRUS
+ssh -l $SERVER_MIKRUS_USER -p $SERVER_MIKRUS_SSH_PORT $SERVER_MIKRUS
