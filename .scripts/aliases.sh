592f1692d37a2cf00359fe44d0b2b6e51e4770aa
Piotr Wittchen <piotr@wittchen.io>
2019-12-22 12:30:44 +0100

    adding new aliases for managing monitors

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+YEAR=$(date +"%Y")
+FINANCE_DIR="~/Documents/finance/budzet/$YEAR"
+
 alias scs="cd ~/.scripts                  && l"
 alias dvp="cd ~/Development               && l"
 alias dox="cd ~/Documents                 && l"
@@ -8,10 +12,12 @@ alias vid="cd ~/Videos                    && l"
 alias mus="cd ~/Music                     && l"
 alias dtf="cd ~/Development/bash/dotfiles && l"
 alias wlp="cd ~/Pictures/wallpapers       && l"
-YEAR=$(date +"%Y")
-FINANCE_DIR="~/Documents/finance/budzet/$YEAR"
 alias kzm="libreoffice $FINANCE_DIR/koszty-zmienne.ods &"
 alias kst="libreoffice $FINANCE_DIR/koszty-stale.ods &"
 alias bdt="libreoffice $FINANCE_DIR/budzet.ods &"
 alias sound_control="pavucontrol"
 alias wifi_list="nmcli dev wifi"
+alias monitor_list="xrandr --listmonitors"
+alias turn_off_laptop_monitor="xrandr --output eDP-1 --off"
+alias reset_monitors="xrandr --auto"
+alias reload_wallpaper="feh --bg-scale ~/Pictures/wallpapers/mountains/bw-mountain-clouds.jpg"
