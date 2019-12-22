df33f621df370ba31d58800de6163c212c5157cc
Piotr Wittchen <piotr@wittchen.io>
2019-12-22 13:41:35 +0100

    updating aqi script on i3 status config

--- .scripts/aqi_log.sh
+++ .scripts/aqi_log.sh
@@ -18,7 +18,7 @@ DESC=$(echo $RESPONSE | jq .current.indexes | jq '.[0]'.description | cut -d "\"
 if [ "$AQI" != "" ] ; then
   case 1 in
     $(($AQI <= 50)))  EMOJI="✔";;
-    $(($AQI <= 75)))  EMOJI="🔺";;
+    $(($AQI <= 75)))  EMOJI="🏭";;
     $(($AQI <= 100))) EMOJI="😷";;
     $(($AQI >= 101))) EMOJI="😱";;
   esac
