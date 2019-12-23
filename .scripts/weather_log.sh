6ae0eaba574698dfbef08d976e97f27c6e01ddfe
Piotr Wittchen <piotr@wittchen.io>
2019-12-23 10:58:15 +0100

    improving weather_log.sh script (not displaying weather info, when wttr.in is down)

--- .scripts/weather_log.sh
+++ .scripts/weather_log.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 /usr/bin/curl --compressed -s "wttr.in/Gliwice?format=+%c+%t+%h+%p+%P+%w" > /var/log/scripts/weather.log
+if grep -q Unknown /var/log/scripts/weather.log; then
+  echo "" > /var/log/scripts/weather.log
+fi
