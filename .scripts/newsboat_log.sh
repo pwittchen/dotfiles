593f5e4baf27996e7ada84db008933ac39c1f652
Piotr Wittchen <piotr@wittchen.io>
2019-12-23 10:39:16 +0100

    improving newsboat_log.sh script: now number of news is updated only when newsboat instance is not running (it's not possible to use 2 instances of newsboat at the same time)

--- .scripts/newsboat_log.sh
+++ .scripts/newsboat_log.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-newsboat -x print-unread | awk '{print $1}' > /var/log/scripts/newsboat.log
+newsboat_is_running=$(ps -ax | grep newsboat | grep -v grep)
+if [ "$newsboat_is_running" == "" ] ; then
+  newsboat -x print-unread | awk '{print $1}' > /var/log/scripts/newsboat.log
+fi
