dcb815ee83bd8125916b348eab56d76dfa83445d
Piotr Wittchen <piotr@wittchen.io>
2019-12-23 16:28:38 +0100

    updating newsboat_log.sh script

--- .scripts/newsboat_log.sh
+++ .scripts/newsboat_log.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-newsboat_is_running=$(ps -ax | grep newsboat | grep -v grep)
-if [ "$newsboat_is_running" == "" ] ; then
-  newsboat -x print-unread | awk '{print $1}' > /var/log/scripts/newsboat.log
-fi
+newsboat -x print-unread | awk '{print $1}' > /var/log/scripts/newsboat.log
