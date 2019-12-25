62f9c8b3f21474d70da1b74b27a1aafdf898dd02
Piotr Wittchen <piotr@wittchen.io>
2019-12-26 00:06:28 +0100

    updating scripts for screen recording

--- .scripts/record_screen_with_mic.sh
+++ .scripts/record_screen_with_mic.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ffmpeg -f x11grab \
 -s $(xdpyinfo | grep dimension | awk '{print $2}') \
--i :0.0 -f alsa -i default ~/Videos/out.mkv
+-i $DISPLAY -f alsa -i default ~/Videos/out.mkv
