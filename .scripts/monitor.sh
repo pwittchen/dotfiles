bf84e9ad899c1f31d74063eed4bb262a9e5de13d
Piotr Wittchen <piotr@wittchen.io>
2020-02-16 20:01:44 +0100

    updating path to the wallpaper - now I keep wallpaper in a path, which doesn't change and in case of changing wallpaper I simply override file in the defined location. It's much more convenient solution than constantly updating path to the wallpaper file in case of change.

--- .scripts/monitor.sh
+++ .scripts/monitor.sh
@@ -17,7 +17,7 @@ function reset {
 }
 
 function wallreload {
-  feh --bg-scale ~/Pictures/wallpapers/mountains/aerial-photography-of-snowy-field-viewing-mountain.jpg 
+  feh --bg-scale ~/Pictures/config/wallpaper/wallpaper.jpg
 }
 
 function help {
