c55fe13626142c06edb12cb95945aa278bc5b853
Piotr Wittchen <piotr@wittchen.io>
2020-03-17 17:42:47 +0100

    reordering commands in setup_external_monitor.sh script

--- .scripts/setup_external_monitor.sh
+++ .scripts/setup_external_monitor.sh
@@ -2,6 +2,6 @@
 
 if [ $(xrandr --listmonitors | wc -l) -eq 3 ]; then
   xrandr --output eDP-1 --off
-  feh --bg-scale ~/Pictures/config/wallpaper/wallpaper.jpg
   i3-msg "workspace 1"
+  feh --bg-scale ~/Pictures/config/wallpaper/wallpaper.jpg
 fi
