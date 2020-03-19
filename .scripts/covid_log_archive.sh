729238b6102e4771020afff7759030cbaf0b020a
Piotr Wittchen <piotr@wittchen.io>
2020-03-19 11:44:15 +0100

    updating covid_log_archive.sh script

--- .scripts/covid_log_archive.sh
+++ .scripts/covid_log_archive.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 stat=$(/usr/bin/python /home/pw/Development/prv/python/covid-19-pl/app.py summary | /usr/bin/tail -n1)
 when=$(date +%Y-%m-%d_%H:%M:%S)
-echo "$when $stat" >> /var/log/scripts/covid_19_pl_archive.log
+echo "$when;$stat" >> /var/log/scripts/covid_19_pl_archive.log
