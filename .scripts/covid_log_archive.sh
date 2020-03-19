114fefc3d46b1624986257b824fc9275e43c9b34
Piotr Wittchen <piotr@wittchen.io>
2020-03-19 13:51:50 +0100

    fixing date formatting in covid_log_archive.sh script

--- .scripts/covid_log_archive.sh
+++ .scripts/covid_log_archive.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 stat=$(/usr/bin/python /home/pw/Development/prv/python/covid-19-pl/app.py summary | /usr/bin/tail -n1)
-when=$(date +%Y-%m-%d %H:%M:%S)
+when=$(date "+%Y-%m-%d %H:%M:%S")
 echo "$when,$stat" | sed "s/;/,/g"  >> /var/log/scripts/covid_19_pl_archive.log
