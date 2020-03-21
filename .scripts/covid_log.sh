6c61dc724a3dbd87dfe137975460bc7400b3331a
Piotr Wittchen <piotr@wittchen.io>
2020-03-21 23:45:52 +0100

    simplifying covid scripts

--- .scripts/covid_log.sh
+++ .scripts/covid_log.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-stats=$(/usr/bin/python /home/pw/Development/prv/python/covid-19-pl/app.py summary | /usr/bin/tail -n1)
-echo $stats | awk -F ";" '{print "☣ " $1 " ☠ " $2 }' > /var/log/scripts/covid_19_pl.log
+python ~/Development/prv/python/covid-19-pl/app.py summary | tail -n1 | awk -F ";" '{print "☣ " $1 " ☠ " $2 }' > /var/log/scripts/covid_19_pl.log
