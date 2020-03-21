5b460c9d5897ae9d6eddd71a85003528fc5f9690
Piotr Wittchen <piotr@wittchen.io>
2020-03-21 22:54:17 +0100

    simplifying covid_log.sh script

--- .scripts/covid_log.sh
+++ .scripts/covid_log.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 stats=$(/usr/bin/python /home/pw/Development/prv/python/covid-19-pl/app.py summary | /usr/bin/tail -n1)
-infected=$(echo $stats | awk --field-separator ";" '{print $1}')
-dead=$(echo $stats | awk --field-separator ";" '{print $2}')
-echo "☣ $infected ☠ $dead" > /var/log/scripts/covid_19_pl.log
+echo $stats | awk -F ";" '{print "☣ " $1 " ☠ " $2 }' > /var/log/scripts/covid_19_pl.log
