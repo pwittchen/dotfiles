02225e155cf643d69c101e2138f873e0d276eb45
Piotr Wittchen <piotr@wittchen.io>
2020-03-26 20:38:07 +0100

    updating scripts_dmenu.sh script, merging scripts for recording screencasts and videos from webcam into one script

--- .scripts/record_screen.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-ffmpeg -f x11grab \
--s $(xdpyinfo | grep dimension | awk '{print $2}') \
--i $DISPLAY ~/Videos/out.mkv
--- .scripts/record_webcam_with_mic.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ffmpeg -i /dev/video0 -f alsa -i default ~/Videos/out.mkv
