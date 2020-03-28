b88e77ce4c8e7b385cc2114f48877350f07530a0
Piotr Wittchen <piotr@wittchen.io>
2020-03-28 23:58:52 +0100

    renaming scripts (making file names consistent)

---
+++ .scripts/set_bg_daytime.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+~/.scripts/set_bg.sh daytime ~/pictures/wallpapers/dynamic/mojave/01_mojave_morning.jpg ~/pictures/wallpapers/dynamic/mojave/02_mojave_midday.jpg ~/pictures/wallpapers/dynamic/mojave/03_mojave_dawn.jpg ~/pictures/wallpapers/dynamic/mojave/04_mojave_night.jpg
+
