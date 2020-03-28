618d833a782077a66b53c41e8d4d17b4db0902b7
Piotr Wittchen <piotr@wittchen.io>
2020-03-28 23:31:04 +0100

    updating scripts for adjusting monitors and wallpapers

--- .scripts/setup_external_monitor.sh
+++ .scripts/setup_external_monitor.sh
@@ -4,5 +4,5 @@ if [ $(xrandr --listmonitors | wc -l) -eq 3 ]; then
   xrandr --output eDP-1 --off
   i3-msg "workspace 1"
   sleep 2 # without it sometimes script didn't work on startup after login
-  feh --bg-scale ~/pictures/config/wallpaper/wallpaper.jpg
+  feh --bg-scale ~/.config/wall.jpg
 fi
