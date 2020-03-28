dd0420b1fc8366835aebaa295e47b51d651c93ce
Piotr Wittchen <piotr@wittchen.io>
2020-03-28 13:13:01 +0100

    adding possibility to set random wallpaper in setbg.sh script

--- .scripts/setbg.sh
+++ .scripts/setbg.sh
@@ -6,6 +6,12 @@ function setbg {
   notify-send "wallpaper changed"
 }
 
+function setrandombg {
+  dir=$(ls -d ~/pictures/wallpapers/*/ | shuf | head -n1)
+  file=$(ls $dir | shuf | head -n1)
+  setbg $dir$file
+}
+
 function help {
     echo "
     setbg.sh is a simple script for setting wallpapers
@@ -13,6 +19,7 @@ function help {
     usage:
 
     setbg.sh <filename>    sets given filename (*.jpg or *.png) as a wallpaper
+    setbg.sh random        sets random wallpaper
     "
 }
 
@@ -23,6 +30,10 @@ function main {
   fi
   if [[ "$1" == *".jpg"* ]] || [[ "$1" == *".png"* ]] ; then
     setbg $1
+    exit
+  fi
+  if [ "$1" == "random" ] ; then
+    setrandombg
     exit
   fi
 }
