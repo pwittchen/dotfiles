790225c714f4ca9cd7d6f10ae9de5b68f585b004
Piotr Wittchen <piotr@wittchen.io>
2020-03-28 23:27:45 +0100

    adding dynamic wallpaper for i3

--- .scripts/setbg_daytime.sh
+++ .scripts/setbg_daytime.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-setbg.sh daytime ~/pictures/wallpapers/dynamic/mojave/01_mojave_morning.jpg ~/pictures/wallpapers/dynamic/mojave/02_mojave_midday.jpg ~/pictures/wallpapers/dynamic/mojave/03_mojave_dawn.jpg ~/pictures/wallpapers/dynamic/mojave/04_mojave_night.jpg
+sleep 1
+~/.scripts/setbg.sh daytime ~/pictures/wallpapers/dynamic/mojave/01_mojave_morning.jpg ~/pictures/wallpapers/dynamic/mojave/02_mojave_midday.jpg ~/pictures/wallpapers/dynamic/mojave/03_mojave_dawn.jpg ~/pictures/wallpapers/dynamic/mojave/04_mojave_night.jpg
 
