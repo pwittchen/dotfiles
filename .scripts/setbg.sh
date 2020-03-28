589d57aec512a36384df4b3d8b15558465e62a2a
Piotr Wittchen <piotr@wittchen.io>
2020-03-28 12:10:32 +0100

    updating setbg.sh script to make it parametrized (in the future I'll add there more options)

--- .scripts/setbg.sh
+++ .scripts/setbg.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
-cp $1 ~/.config/wall.jpg
-feh --bg-scale ~/.config/wall.jpg
-notify-send "wallpaper changed"
+function setbg {
+  cp $1 ~/.config/wall.jpg
+  feh --bg-scale ~/.config/wall.jpg
+  notify-send "wallpaper changed"
+}
+
+function help {
+    echo "
+    setbg.sh is a simple script for setting wallpapers
+
+    usage:
+
+    setbg.sh <filename>
+    "
+}
+
+function main {
+  if [ -z "$1" ] || [ "$1" == "help" ] ; then
+    help
+    exit
+  fi
+  if [[ "$1" == *".jpg"* ]] || [[ "$1" == *".png"* ]] ; then
+    setbg $1
+    exit
+  fi
+}
+
+main "$@"
