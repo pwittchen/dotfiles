9fc2a9acac8bc36e0bfd5b32a5e00eab12f2c996
Piotr Wittchen <piotr@wittchen.io>
2020-03-29 10:25:25 +0200

    updating paths to daytime wallpapers in set_bg_daytime.sh script

--- .scripts/set_bg_daytime.sh
+++ .scripts/set_bg_daytime.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-~/.scripts/set_bg.sh daytime ~/pictures/wallpapers/dynamic/mojave/01_mojave_morning.jpg ~/pictures/wallpapers/dynamic/mojave/02_mojave_midday.jpg ~/pictures/wallpapers/dynamic/mojave/03_mojave_dawn.jpg ~/pictures/wallpapers/dynamic/mojave/04_mojave_night.jpg
-
+~/.scripts/set_bg.sh daytime ~/.config/wall_01.jpg ~/.config/wall_02.jpg ~/.config/wall_03.jpg ~/.config/wall_04.jpg
