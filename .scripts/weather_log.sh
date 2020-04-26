55ecc47be2494703dca6392e0fd3c6b85975cf7e
Piotr Wittchen <piotr@wittchen.io>
2020-04-26 21:11:26 +0200

    updating weather_log.sh script

--- .scripts/weather_log.sh
+++ .scripts/weather_log.sh
@@ -2,7 +2,7 @@
 
 source ~/.config/scripts/weather.conf
 
-curl --compressed -s "wttr.in/$LOCATION?format=+%c+%t+%h+%p+%P+%w" > /var/log/scripts/weather.log
+curl --compressed -s "wttr.in/$LOCATION?format=+%c+%t+%h+%p+%P+%w" | head -n1 > /var/log/scripts/weather.log
 grep -q Unknown /var/log/scripts/weather.log && echo "" > /var/log/scripts/weather.log
 grep -q Sorry /var/log/scripts/weather.log && echo "" > /var/log/scripts/weather.log
 grep -q Error /var/log/scripts/weather.log && echo "" > /var/log/scripts/weather.log
