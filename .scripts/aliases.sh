d9a84a074b0349ce09415b4a238855497828faed
Piotr Wittchen <piotr@wittchen.io>
2020-04-29 16:04:19 +0200

    updating aliases: simplifying things, removing link to personal docs and spreadsheets, removing listing files

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-YEAR=$(date +"%Y")
-FINANCE_DIR="~/documents/finance/budzet/$YEAR"
-
-alias scs="cd ~/.scripts                  	&& l"
-alias dvp="cd ~/development                     && l"
-alias dox="cd ~/documents                 	&& l"
-alias dld="cd ~/downloads                 	&& l"
-alias pix="cd ~/pictures                  	&& l"
-alias vid="cd ~/videos                   	&& l"
-alias dtf="cd ~/development/prv/bash/dotfiles 	&& l"
-alias wlp="cd ~/pictures/wallpapers       	&& l"
-alias kzm="libreoffice $FINANCE_DIR/koszty-zmienne.ods &"
-alias kst="libreoffice $FINANCE_DIR/koszty-stale.ods &"
-alias bdt="libreoffice $FINANCE_DIR/budzet.ods &"
+alias scs="cd ~/.scripts"
+alias dvp="cd ~/development"
+alias dox="cd ~/documents"
+alias dld="cd ~/downloads"
+alias pix="cd ~/pictures"
+alias vid="cd ~/videos"
+alias dtf="cd ~/development/prv/bash/dotfiles"
+alias wlp="cd ~/pictures/wallpapers"
 alias mnt_usb="sudo mount /dev/sdb1 /media/usb"
 alias umt_usb="sudo umount /media/usb"
