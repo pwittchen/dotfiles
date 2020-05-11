e9ed0dd9775edf074071ea92b247919ddc7d41f3
Piotr Wittchen <piotr@wittchen.io>
2020-05-11 14:01:01 +0200

    updating scripts

--- .scripts/set_bg_daynight.sh
+++ .scripts/set_bg_daynight.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 ~/.scripts/set_bg.sh daynight ~/.config/wall_01.jpg ~/.config/wall_02.jpg ~/.config/wall_03.jpg ~/.config/wall_04.jpg
+sleep 1
 /usr/bin/betterlockscreen -u ~/.config/wall.jpg
