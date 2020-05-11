7df3058ea4aed834630696654e04d1d70c78034f
Piotr Wittchen <piotr@wittchen.io>
2020-05-11 20:35:32 +0200

    updating set_bg_daynight.sh script

--- .scripts/set_bg_daynight.sh
+++ .scripts/set_bg_daynight.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 ~/.scripts/set_bg.sh daynight ~/.config/wall_01.jpg ~/.config/wall_02.jpg ~/.config/wall_03.jpg ~/.config/wall_04.jpg
 sleep 1
+/usr/bin/rm -rf ~/.cache/i3lock/current/*
 /usr/bin/betterlockscreen -u ~/.config/wall.jpg
