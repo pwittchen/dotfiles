ce5e54774bf8c05b4f412dd0722dac5760b7202c
Piotr Wittchen <piotr@wittchen.io>
2020-07-06 18:37:12 +0200

    adding comment in set_external_monitor.sh script

--- .scripts/set_external_monitor.sh
+++ .scripts/set_external_monitor.sh
@@ -4,5 +4,5 @@ if [ $(xrandr --listmonitors | wc -l) -eq 3 ]; then # run instructions only when
   xrandr --output eDP-1 --off # turn off the primary (laptop) monitor
   i3-msg "workspace 1" # switch to the workspace 1 (secondary monitor is on the workspace 2 in the beginning)
   sleep 2 # without it sometimes script didn't work on startup after login
-  feh --bg-scale ~/.config/wall.jpg
+  feh --bg-scale ~/.config/wall.jpg # reload wallpaper to adjust it to the screen size
 fi
