98f8cddf119233e7a72ff327291cedecd093cbb4
Piotr Wittchen <piotr@wittchen.io>
2021-01-09 12:54:48 +0100

    updating setup regarding switching between laptop monitor and external screen

--- .scripts/set_laptop_monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $(xrandr --listmonitors | wc -l) -eq 2 ]; then # run instructions only when no external monitor is connected
-  xrandr --auto # reset default settings
-  sleep 2 # without it sometimes script didn't work on startup after login
-  cp ~/.config/wall_laptop_monitor.png ~/.config/wall.png # copy appropriate wallaper
-  cp ~/.config/wall_laptop_monitor_lock.png ~/.cache/i3lock/current/l_resized.png # set appropriate lock screen
-  feh --bg-scale ~/.config/wall.png # reload wallpaper to adjust it to the screen size
-fi
--- .scripts/set_monitor.sh
+++ .scripts/set_monitor.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# setup for laptop screen
+
+if [ $(xrandr --listmonitors | wc -l) -eq 2 ]; then # run instructions only when no external monitor is connected
+  xrandr --auto # reset default settings
+  sleep 2 # without it sometimes script didn't work on startup after login
+  cp ~/.config/wall_laptop_monitor.png ~/.config/wall.png # copy appropriate wallaper
+  cp ~/.config/wall_laptop_monitor_lock.png ~/.cache/i3lock/current/l_resized.png # set appropriate lock screen
+  feh --bg-scale ~/.config/wall.png # reload wallpaper to adjust it to the screen size
+fi
+
+# setup for external connected screeen
+
 if [ $(xrandr --listmonitors | wc -l) -eq 3 ]; then # run instructions only when external monitor is connected
   xrandr --output eDP-1 --off # turn off the primary (laptop) monitor
   i3-msg "workspace 1" # switch to the workspace 1 (secondary monitor is on the workspace 2 in the beginning)
