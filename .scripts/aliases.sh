e52f0e66526c1c62602f0a19bcaf61f788176d38
Piotr Wittchen <piotr@wittchen.io>
2021-05-17 23:16:28 +0200

    updating aliases

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 alias v="vim"
+alias s="systemctl"
 alias scs="cd ~/.scripts"
 alias dvp="cd ~/development"
 alias dox="cd ~/documents"
@@ -9,5 +10,5 @@ alias pix="cd ~/pictures"
 alias vid="cd ~/videos"
 alias dtf="cd ~/development/prv/bash/dotfiles"
 alias wlp="cd ~/pictures/wallpapers"
-alias mnt_usb="sudo mount /dev/sdb1 /media/usb"
-alias umt_usb="sudo umount /media/usb"
+alias usb_in="sudo mount /dev/sdb1 /media/usb"
+alias usb_out="sudo umount /media/usb"
