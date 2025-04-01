a45f02eeaedb1462c9ee6b485521cfd8740f3c20
Piotr Wittchen <piotr@wittchen.io>
2025-04-01 21:51:06 +0200

    updated aliases.sh

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -2,15 +2,17 @@
 
 alias v="vim"
 alias V="sudo vim"
-alias s="sudo systemctl"
+alias jsn="python3 -m json.tool"
 alias scs="cd ~/.scripts"
 alias dvp="cd ~/development"
 alias dox="cd ~/documents"
 alias dld="cd ~/downloads"
 alias pix="cd ~/pictures"
-alias vid="cd ~/videos"
+alias mvs="cd ~/movies"
 alias dtf="cd ~/development/prv/bash/dotfiles"
 alias wlp="cd ~/pictures/wallpapers"
+
+# linux aliases
 alias usb_in="sudo mount /dev/sdb1 /media/usb"
 alias usb_out="sudo umount /media/usb"
 alias sd_in="sudo mount /dev/sda1 /media/sdcard"
