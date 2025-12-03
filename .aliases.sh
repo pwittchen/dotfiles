8233c8a45f1766e91541afcb3d032e8ca1812891
Piotr Wittchen <piotr@wittchen.io>
2025-12-03 20:56:58 +0100

    removed ai-links alias

--- .aliases.sh
+++ .aliases.sh
@@ -11,4 +11,3 @@ alias mvs="cd ~/movies"
 alias dtf="cd ~/development/prv/bash/dotfiles"
 alias wlp="cd ~/pictures/wallpapers"
 alias sshm="ssh root@marek245.mikrus.xyz -p10245"
-alias ai-links='ln -sf .ai/prompts/CLAUDE.md CLAUDE.md && ln -sf .ai/prompts/AGENTS.md AGENTS.md'
